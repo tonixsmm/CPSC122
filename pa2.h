82e4eb025ea93a47bfdd0e6d6b31b0ca1855f64d
Tony Nguyen <tonixsmm>
2023-02-07 12:48:54 -0800

    working on sortAnalysis

--- pa2.h
+++ pa2.h
@@ -4,13 +4,16 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+using namespace std::chrono;
 
 void createVector(vector<int>& vec, int size, bool sorting, bool desc);
 void printVector(vector<int> vec);
 void swap(int& a, int& b);
 bool sortedChecking(vector<int> vec);
+void sortAnalysis(vector<int> vec);
 vector<int> selectionSort(vector<int> vec);
 vector<int> bubbleSort(vector<int> vec);
 vector<int> insertionSort(vector<int> vec);
