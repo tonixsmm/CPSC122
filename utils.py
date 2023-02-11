68c062205ade5847544ef69f9716c131bd7b1245
Tony Nguyen <tonixsmm>
2023-02-10 22:22:10 -0800

    done with formatting

--- utils.py
+++ utils.py
@@ -4,12 +4,20 @@
 # Programming Assignment: PA2
 # Description: This program implements several sorting algorithm to compare for efficiency
 # Note: I decided to use milliseconds instead of seconds for execution time measurement becuase it is more accurate
-# Note: This utility file is adapted from my DA5 and DA6 submission, CPSC 222, Fall 2022, by Dr. Gina Sprint
+# Note: This utility file is adapted from my DA5 and DA6 submission in CPSC 222, Fall 2022, by Dr. Gina Sprint
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Function: data_loading()
+# Date Created: 02/10/2022
+# Date Last Modified: 02/10/2022
+# Description: This function loads a CSV file into a DataFrame and prepares it for plotting graphs
+# Input parameters: File name
+# Returns: a DataFrame
+# Pre: Must be a CSV file
+# Post: None
 def data_loading(filename):
     data = pd.read_csv(filename)
     
@@ -26,10 +34,18 @@ def data_loading(filename):
 
     return data
 
+# Function: draw_plot()
+# Date Created: 02/10/2022
+# Date Last Modified: 02/10/2022
+# Description: This function plots line charts for the given DataFrame
+# Input parameters: Four DataFrames, sorting configuration, and attribute to plot
+# Returns: None
+# Pre: Variables for sorting configuration and attribute to plot are case sensitive
+# Post: None
 def draw_plot(df_selection, df_bubble, df_insertion, df_shell, sort_type, graph_type):
-
     plt.figure(figsize=(10, 8))
 
+    # Plot
     plt.plot([1, 2, 3, 4], df_selection[df_selection["Sort type"] == sort_type][graph_type],'o-', label="Selection")
     plt.plot([1, 2, 3, 4], df_bubble[df_bubble["Sort type"] == sort_type][graph_type],'v-', label="Bubble")
     plt.plot([1, 2, 3, 4], df_insertion[df_insertion["Sort type"] == sort_type][graph_type],'s-', label="Insertion")
