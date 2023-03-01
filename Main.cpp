1ee225135f2262db55ff8242d9ed712fb2c28124
Tony Nguyen <tonixsmm>
2023-03-01 13:47:14 -0800

    remove bonus functions calling

--- Main.cpp
+++ Main.cpp
@@ -5,11 +5,5 @@ int main(int argc, char * argv[]) {
 	checkInputFile(argc);
 	runReadFromFile(argv[1]);
 	
-	cout << endl;
-	cout << endl;
-	cout << "Bonus Part" << endl;
-	cout << "----------" << endl;
-
-	runBonus();
 	return 0;
 }
