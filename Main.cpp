47c7f56a5f44425a6fa2237da50c2849f090d27b
Tony Nguyen <tonixsmm>
2023-05-06 15:51:10 -0700

    uncomment runTasks

--- Main.cpp
+++ Main.cpp
@@ -11,11 +11,11 @@ I attempted the ART Review Bonus
 
 // no need for any changes here
 int main() {
-	// runTask1();
-	// runTask2();
+	runTask1();
+	runTask2();
 	runTask3();
-	// runTask4();
-	// runTask5();
+	runTask4();
+	runTask5();
 	
 	// runBONUSTask();
 
